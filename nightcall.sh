5a4a9fa7bbdea85d94b9f8826543c2c94bdc0f8f
krachzack <hello@phstadler.com>
2018-06-23 22:10:02 +0200

    Trying way better quality

--- nightcall.sh
+++ nightcall.sh
@@ -4,6 +4,8 @@
 # ====================
 STREAM_FILE="/home/pi/nightcall/snore.wav" # File to stream as absolute path or relative to directory of this script
 APT_GET_DEPS="vlc-nox"         # These space-separated packages will be installed if the VLC tool cvlc is not available
+SAMPLING_RATE=44100
+KBPS="8k"
 
 if [ "$HOSTNAME" = zenzi ]; then
     # zenzi only configuration
@@ -38,4 +40,4 @@ cd "$(dirname "$0")"
 cvlc rtp://@:$STREAM_LISTEN_PORT &
 
 # Stream as 8kbps MP3
-cvlc -vvv file://$STREAM_FILE  --loop --file-caching=1000 --sout="#transcode{vcodec=none,acodec=mp3,ab=8k,samplerate=22050,channels=1}:rtp{dst=$STREAM_TARGET_ADDR,port=$STREAM_TARGET_PORT,name=$STREAM_NAME,ttl=3,mux=ts,sdp=sap}"
+cvlc -vvv file://$STREAM_FILE --repeat --file-caching=1000 --sout="#transcode{vcodec=none,acodec=mp3,ab=$KBPS,samplerate=$SAMPLING_RATE,channels=1}:rtp{dst=$STREAM_TARGET_ADDR,port=$STREAM_TARGET_PORT,name=$STREAM_NAME,ttl=3,mux=ts,sdp=sap}"
