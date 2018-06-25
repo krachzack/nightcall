66562d14e60dd1e72c098af33fbb5f044e548979
krachzack <hello@phstadler.com>
2018-06-25 22:14:36 +0200

    Added caching variables set to 100ms

--- nightcall.sh
+++ nightcall.sh
@@ -4,8 +4,10 @@
 # ====================
 STREAM_URL="alsa://plughw:1,0" # Alsa microphone to stream from
 APT_GET_DEPS="vlc-nox"         # These space-separated packages will be installed if the VLC tool cvlc is not available
-SAMPLING_RATE=22500
-KBPS="16k"
+SAMPLING_RATE=12000
+KBPS="1k"
+RECEIVE_CACHE_MS=100
+SEND_CACHE_MS=100
 
 if [ "$HOSTNAME" = zenzi ]; then
     # zenzi only configuration
@@ -37,7 +39,10 @@ fi
 cd "$(dirname "$0")"
 
 # Start receiving stream in the background
-cvlc rtp://@:$STREAM_LISTEN_PORT &
+cvlc rtp://@:$STREAM_LISTEN_PORT --network-caching=$RECEIVE_CACHE_MS &
 
 # Stream as 8kbps MP3
-cvlc -vvv $STREAM_URL --repeat --file-caching=1000 --sout="#transcode{vcodec=none,acodec=mp3,ab=$KBPS,samplerate=$SAMPLING_RATE,channels=1}:rtp{dst=$STREAM_TARGET_ADDR,port=$STREAM_TARGET_PORT,name=$STREAM_NAME,ttl=3,mux=ts,sdp=sap}"
+cvlc -vvv $STREAM_URL --repeat \
+  --file-caching=$SEND_CACHE_MS \
+  --live-caching=$SEND_CACHE_MS \ # Microphone caching in millisconds
+  --sout="#transcode{vcodec=none,acodec=mp3,ab=$KBPS,samplerate=$SAMPLING_RATE,channels=1}:rtp{dst=$STREAM_TARGET_ADDR,port=$STREAM_TARGET_PORT,name=$STREAM_NAME,ttl=3,mux=ts,sdp=sap}"
