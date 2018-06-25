83f3a8f1062eb308f4fdfd98c8f1ccf2c66cd3e3
krachzack <hello@phstadler.com>
2018-06-25 23:25:54 +0200

    Added rtsp-caching and network-caching to both ends
    Also increased bitrate to 16k

--- nightcall.sh
+++ nightcall.sh
@@ -5,7 +5,7 @@
 STREAM_URL="alsa://plughw:1,0" # Alsa microphone to stream from
 APT_GET_DEPS="vlc-nox"         # These space-separated packages will be installed if the VLC tool cvlc is not available
 SAMPLING_RATE=12000
-KBPS="1k"
+KBPS="16k"
 # Caching on receiving end in millisconds
 RECEIVE_CACHE_MS=100
 # Microphone caching before sending in millisconds
@@ -41,10 +41,14 @@ fi
 cd "$(dirname "$0")"
 
 # Start receiving stream in the background
-cvlc rtp://@:$STREAM_LISTEN_PORT --network-caching=$RECEIVE_CACHE_MS &
+cvlc rtp://@:$STREAM_LISTEN_PORT \
+  --network-caching=$RECEIVE_CACHE_MS \
+  --rtsp-caching=$RECEIVE_CACHE_MS &
 
 # Stream as 8kbps MP3
 cvlc -vvv $STREAM_URL --repeat \
   --file-caching=$SEND_CACHE_MS \
   --live-caching=$SEND_CACHE_MS \
+  --rtsp-caching=$SEND_CACHE_MS \
+  --network-caching=$SEND_CACHE_MS \
   --sout="#transcode{vcodec=none,acodec=mp3,ab=$KBPS,samplerate=$SAMPLING_RATE,channels=1}:rtp{dst=$STREAM_TARGET_ADDR,port=$STREAM_TARGET_PORT,name=$STREAM_NAME,ttl=3,mux=ts,sdp=sap}"
