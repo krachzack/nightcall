694d86f78c20d46253d97c0b4883da6d84b0c609
krachzack <hello@phstadler.com>
2018-07-10 18:03:56 +0200

    Added virtual sink for remote speech

--- pulseaudio.sh
+++ pulseaudio.sh
@@ -68,7 +68,14 @@ function patch_pulse_config {
   uncomment $PULSE_DEFAULT_PA "module-zeroconf-publish"
   # sudo bash -c "echo 'resample-method = speex-float-3' >> $PULSE_DEFAULT_PA"
 
-  # Disable autospawn if not already enabled
+  # Add sink for remote stream so it can be muted
+  if ! grep autospawn=no "$PULSE_DEFAULT_PA"
+  then
+    sudo bash -c "echo 'load-module module-null-sink sink_name=remotephone sink_properties=device.description=\"Remote_Phone\"' >> $PULSE_DEFAULT_PA"
+    sudo bash -c "echo 'load-module module-loopback source=remotephone.monitor sink=alsa_output.usb-C-Media_Electronics_Inc._USB_Audio_Device-00.analog-stereo latency_msec=50' >> $PULSE_DEFAULT_PA"
+  fi
+
+  # Disable autospawn if not already disabled
   if ! grep autospawn=no "$PULSE_CLIENT_CONF"
   then
     echo "Patching $PULSE_CLIENT_CONF ..."
